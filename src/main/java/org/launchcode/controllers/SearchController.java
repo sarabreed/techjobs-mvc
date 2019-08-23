450590c429a4830b91f8237c5d563a189ea63b1c
Sara Breed <sara.n..breed@gmail.com>
2019-08-22 20:10:09 -0500

    Final Assignment

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -24,4 +24,39 @@ public class SearchController {
 
     // TODO #1 - Create handler to process search request and display results
 
+    @RequestMapping(value = "results")
+    public String search(@RequestParam String searchType, @RequestParam String searchTerm, Model model) {
+        Iterable<HashMap<String, String>> jobs;
+        if (searchType.equals("all")) {
+            jobs = JobData.findByValue(searchTerm);
+        }
+        else {
+            jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+            model.addAttribute("selectedColumn", searchType);
+        }
+
+        model.addAttribute("columns", ListController.columnChoices);
+        model.addAttribute("jobs", jobs);
+
+        return "search";
+    }
+
+
+//    @RequestMapping(value = "search")
+//    public String searchResults(Model model, @RequestParam String column, @RequestParam String value) {
+//        if (column.equals("all")) {
+//            ArrayList<HashMap<String, String>> jobs = JobData.findByValue(value);
+//            model.addAttribute("title", "All Jobs");
+//            model.addAttribute("jobs", jobs);
+//            return "search-all";
+//        } else {
+//            ArrayList<HashMap<String, String>> jobs = JobData.findByColumnAndValue(column, value);
+//            model.addAttribute("title", "All " + ListController.columnChoices.get(column) + " Values");
+//            model.addAttribute("column", column);
+//            model.addAttribute("jobs", jobs);
+//            return "search-column";
+//        }
+//    }
+
+
 }
